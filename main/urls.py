7e6eba4f70377f9d32ef8322e576612c9d7ede9a
Vikshita-R <123354916+Vikshita-R@users.noreply.github.com>
2024-06-22 23:11:02 +0530

    edit page completed

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("student_dashboard/", views.student_dashboard, name="student_dashboard"),
-    
+    path("teacher_dashboard/",views.teacher_dashboard, name="teacher_dashboard"),
+    # path("teacher_dashboard/add_marks/", views.add_marks, name="add_marks"),
+    path("teacher_dashboard/edit_marks/", views.edit_marks, name="edit_marks"),
+    # path("teacher_dashboard/view_marks/", views.view_marks, name="view_marks"),
 ]
